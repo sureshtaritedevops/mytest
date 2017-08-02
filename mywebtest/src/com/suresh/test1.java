28a50dc5ab569a0574292c127aab975a0fcd7f1f
divyataritedevops <divyataritedevops@gmail.com>
2017-08-02 10:49:56 +0530

    remove comments

--- mywebtest/src/com/suresh/test1.java
+++ mywebtest/src/com/suresh/test1.java
@@ -2,17 +2,9 @@ package com.suresh;
 
 public class test1 {
 
-//change suresh change suresh
 
 	public static void main(String[] args) {
 
-//change line
-System.out.println("hello1"); 
 
-	}
-
-//change a
-		
-	//change B
 		
 }
